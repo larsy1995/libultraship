ea2cb838fce927356715afa1f2b314b9bb93939d
Lars-Christian Selland HVL <larsy1995@gmail.com>
2025-02-03 02:29:54 +0100

    Update gfx_sdl2.cpp

--- src/graphic/Fast3D/gfx_sdl2.cpp
+++ src/graphic/Fast3D/gfx_sdl2.cpp
@@ -369,13 +369,6 @@ static void gfx_sdl_init(const char* game_name, const char* gfx_api_name, bool s
     }
 
     wnd = SDL_CreateWindow(title, posX, posY, window_width, window_height, flags);
-    int actual_w, actual_h;
-    SDL_GetRendererOutputSize(renderer, &actual_w, &actual_h);
-
-    // If the window size is larger than expected, adjust it
-    if (actual_w > window_width * 1.5 || actual_h > window_height * 1.5) {
-        SDL_SetWindowSize(wnd, window_width, window_height);
-    }
 #ifdef _WIN32
     // Get Windows window handle and use it to subclass the window procedure.
     // Needed to circumvent SDLs DPI scaling problems under windows (original does only scale *sometimes*).
@@ -394,7 +387,7 @@ static void gfx_sdl_init(const char* game_name, const char* gfx_api_name, bool s
     }
 
     if (use_opengl) {
-            SDL_GetWindowSize(wnd, &window_width, &window_height);
+        SDL_GL_GetDrawableSize(wnd, &window_width, &window_height);
 
         if (start_in_fullscreen) {
             set_fullscreen(true, false);
@@ -497,9 +490,8 @@ static void gfx_sdl_set_mouse_callbacks(bool (*on_btn_down)(int btn), bool (*on_
 }
 
 static void gfx_sdl_get_dimensions(uint32_t* width, uint32_t* height, int32_t* posX, int32_t* posY) {
-    SDL_GetWindowSize(wnd, reinterpret_cast<int*>(width), reinterpret_cast<int*>(height));  // Logical size
-    SDL_GetRendererOutputSize(renderer, reinterpret_cast<int*>(width), reinterpret_cast<int*>(height)); // Render size
-    SDL_GetWindowPosition(wnd, reinterpret_cast<int*>(posX), reinterpret_cast<int*>(posY));
+    SDL_GL_GetDrawableSize(wnd, static_cast<int*>((void*)width), static_cast<int*>((void*)height));
+    SDL_GetWindowPosition(wnd, static_cast<int*>(posX), static_cast<int*>(posY));
 }
 
 static int translate_scancode(int scancode) {
@@ -575,8 +567,7 @@ static void gfx_sdl_handle_single_event(SDL_Event& event) {
         case SDL_WINDOWEVENT:
             switch (event.window.event) {
                 case SDL_WINDOWEVENT_SIZE_CHANGED:
-                    SDL_GetWindowSize(wnd, &window_width, &window_height);  // Get logical size
-                    SDL_GetRendererOutputSize(renderer, &window_width, &window_height);  // Get actual Metal render size
+                    SDL_GL_GetDrawableSize(wnd, &window_width, &window_height);
                     break;
                 case SDL_WINDOWEVENT_CLOSE:
                     if (event.window.windowID == SDL_GetWindowID(wnd)) {
