698e2a9066735134fd02e78c1afb7635ce288f43
Lars-Christian Selland HVL <larsy1995@gmail.com>
2025-02-03 02:51:39 +0100

    Update gfx_sdl2.cpp

--- src/graphic/Fast3D/gfx_sdl2.cpp
+++ src/graphic/Fast3D/gfx_sdl2.cpp
@@ -385,10 +385,23 @@ static void gfx_sdl_init(const char* game_name, const char* gfx_api_name, bool s
         posX = 100;
         posY = 100;
     }
+    int drawable_width, drawable_height;
+    float scale_factor = 1.0f;
 
     if (use_opengl) {
-        SDL_GL_GetDrawableSize(wnd, &window_width, &window_height);
+        SDL_GL_GetDrawableSize(wnd, &drawable_width, &drawable_height);
+    } else {
+        SDL_GetRendererOutputSize(renderer, &drawable_width, &drawable_height);
+    }
 
+    SDL_GetWindowSize(wnd, &window_width, &window_height);
+    scale_factor = (float)drawable_width / (float)window_width;
+
+    // Apply scale correction
+    window_width = drawable_width / scale_factor;
+    window_height = drawable_height / scale_factor;
+    SDL_SetWindowSize(wnd, window_width, window_height);
+    if (use_opengl) {
         if (start_in_fullscreen) {
             set_fullscreen(true, false);
         }
