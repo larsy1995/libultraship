58d66dd7be4277ed06ba6078e6dac2fce6220d04
Lars-Christian Selland <larsy1995@gmail.com>
2025-02-04 05:28:09 +0100

    Update Gui.cpp

--- src/window/gui/Gui.cpp
+++ src/window/gui/Gui.cpp
@@ -652,7 +652,6 @@ void Gui::DrawGame() {
                 size = ImVec2(sWdth, sHght);
             }
         } else { // in pixel perfect mode it's much easier
-            const int scalingFactor = retina_factor();
             const int factor = GetIntegerScaleFactor();
             float sPosX = floor(size.x / 2.0f - (gfx_current_dimensions.width * factor) / 2.0f);
             float sPosY = floor(size.y / 2.0f - (gfx_current_dimensions.height * factor) / 2.0f);
